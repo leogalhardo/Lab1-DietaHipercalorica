6c07232110e2d2a7312572b0d2a5c6f21ff8bd80
Leonardo Galhardo <leomgal20@hotmail.com>
2023-03-29 15:44:21 -0300

    Changing configuration

--- R/Analise FETO GERAL.R
+++ R/Analise FETO GERAL.R
@@ -1,30 +1,45 @@
+install.packages("ggplot2")
+install.packages("ggpubr")
+install.packages("pacman")
+library(pacman)
 library(ggplot2)
+library(ggpubr)
 library(tidyverse)
 
-# ANÁLISE TABELA FETO GERAL
+#################### ANÁLISE TABELA FETO GERAL ###############################
 
-## VARIÁVEL `(%) CEREBRO`
+##### VARIÁVEL `(%) CEREBRO`
 
-### GRÁFICO DE FREQUÊNCIAS POR DENSIDADE, SEPARANDO TRATAMENTOS DIFERENTES POR COR
+## GRÁFICO DE FREQUÊNCIAS POR DENSIDADE, SEPARANDO TRATAMENTOS DIFERENTES POR COR
 
 ggplot(data = FETO_GERAL, mapping = aes(x = `(%) CEREBRO`, y = ..density..)) +
   geom_freqpoly(mapping = aes(colour = `TRATAMENTO`))
 
-#### ELIMINANDO OUTLIER
+# ELIMINANDO OUTLIER
 
 sem_out <- filter(FETO_GERAL, `(%) CEREBRO` < 0.4)
   
 ggplot(data = sem_out, mapping = aes(x = `(%) CEREBRO`, y = ..density..)) +
   geom_freqpoly(mapping = aes(colour = `TRATAMENTO`))
 
-### BOXPLOT
+## BOXPLOT
 
 ggplot(data = FETO_GERAL, mapping = aes(x = `TRATAMENTO`, y = `(%) CEREBRO`)) +
   geom_boxplot()
 
-#### ELIMINANDO OUTLIER
+# ELIMINANDO OUTLIER
 
 ggplot(data = sem_out, mapping = aes(x = `TRATAMENTO`, y = `(%) CEREBRO`)) +
   geom_boxplot()
 
-### 
+##  GRÁFICO DE BARRAS DE MÉDIAS E ERROS
+
+p_load(tidyverse, ggplot2, ggpubr)
+
+ggplot(data = FETO_GERAL) +
+  geom_errorbar(mapping = aes(x = `TRATAMENTO`, y = `(%) CEREBRO`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `TRATAMENTO`, y = `(%) CEREBRO`),
+           stat = "summary",
+           fun = "mean")
