8fbd62a310b08a64d5ff5059a3e9047a90d55a0c
Leonardo Galhardo <leomgal20@hotmail.com>
2023-03-29 16:40:44 -0300

    Anova and Bonferroni

--- R/Analise FETO GERAL.R
+++ R/Analise FETO GERAL.R
@@ -1,10 +1,14 @@
 install.packages("ggplot2")
 install.packages("ggpubr")
 install.packages("pacman")
+library(remotes)
+install_github("fndemarqui/planex", force = TRUE)
+library(planex)
 library(pacman)
 library(ggplot2)
 library(ggpubr)
 library(tidyverse)
+library(agricolae)
 
 #################### ANÁLISE TABELA FETO GERAL ###############################
 
@@ -20,7 +24,7 @@ ggplot(data = FETO_GERAL, mapping = aes(x = `(%) CEREBRO`, y = ..density..)) +
 sem_out <- filter(FETO_GERAL, `(%) CEREBRO` < 0.4)
   
 ggplot(data = sem_out, mapping = aes(x = `(%) CEREBRO`, y = ..density..)) +
-  geom_freqpoly(mapping = aes(colour = `TRATAMENTO`))
+  geom_freqpoly(mapping = aes(colour = `TRATAMENTO`), size = 1.5)
 
 ## BOXPLOT
 
@@ -32,14 +36,25 @@ ggplot(data = FETO_GERAL, mapping = aes(x = `TRATAMENTO`, y = `(%) CEREBRO`)) +
 ggplot(data = sem_out, mapping = aes(x = `TRATAMENTO`, y = `(%) CEREBRO`)) +
   geom_boxplot()
 
-##  GRÁFICO DE BARRAS DE MÉDIAS E ERROS
+## GRÁFICO DE BARRAS DE MÉDIAS E ERROS - sem outlier
 
 p_load(tidyverse, ggplot2, ggpubr)
 
-ggplot(data = FETO_GERAL) +
+ggplot(data = sem_out) +
   geom_errorbar(mapping = aes(x = `TRATAMENTO`, y = `(%) CEREBRO`), 
                 stat = "summary",
                 fun.data = "mean_se") +
   geom_bar(mapping = aes(x = `TRATAMENTO`, y = `(%) CEREBRO`),
            stat = "summary",
            fun = "mean")
+
+## ANOVA - sem outlier
+
+mod <- aov(`(%) CEREBRO` ~ `TRATAMENTO`, data = sem_out)
+summary(mod)
+
+## TESTE DE BONFERRONI - COMPARAÇOES MULTIPLAS
+
+bonferroni <- LSD.test(mod, "TRATAMENTO", p.adj="bonferroni", group=TRUE)
+print(bonferroni)
+
