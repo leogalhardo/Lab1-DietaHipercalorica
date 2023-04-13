f016fefc8613d4d64fadf5c598c53579a8a6f522
VictorHugoLMoreira <victorh.lmoreira@yahoo.com>
2023-04-12 21:38:42 -0300

    atualizando

--- R/Analise FETO VICTOR.R
+++ R/Analise FETO VICTOR.R
@@ -32,6 +32,9 @@ dados_mae <- read_excel("RESULTADOS alterados 03-04.xlsx",
 
 
 
+#### PLANILHA FETO GERAL ###########
+
+
 ## --------- Gráfico de correlação entre as variáveis --------##
 
 cor <- dados_feto %>% select(`Placenta-PESO`,PESO,`Eficiência Placentária`,
@@ -269,7 +272,6 @@ pairwise.wilcox.test(dados_feto$PESO, factor(dados_feto$TRATAMENTO),
 
 
 
-
 # Testando `Eficiência Placentária` por TRATAMENTO (Paramétrico)
 mod <- aov(`Eficiência Placentária` ~ `TRATAMENTO`, data = dados_feto)
 anova(mod)# os grupos apresentam diferença significativa
@@ -294,6 +296,445 @@ kruskal.test(`(%) CEREBRO` ~ factor(TRATAMENTO),dados_feto) # os grupos apresent
 
 pairwise.wilcox.test(dados_feto$`(%) CEREBRO`, factor(dados_feto$TRATAMENTO),
                      p.adjust.method = "none")
+
+
+#Testando `CEREBRO` por TRATAMENTO (Não- paramétrico)
+
+kruskal.test(`CEREBRO` ~ factor(TRATAMENTO),dados_feto) # os grupos não apresentam diferença significativa
+
+
+
+# Testando `(%) FÍGADO` por TRATAMENTO (Paramétrico)
+mod <- aov(`(%) FÍGADO` ~ `TRATAMENTO`, data = dados_feto)
+anova(mod)
+
+bonferroni <- LSD.test(mod, "TRATAMENTO", p.adj="bonferroni", group=TRUE)
+print(bonferroni)
+
+
+#Testando FÍGADO por TRATAMENTO (Não- paramétrico)
+
+kruskal.test(FÍGADO ~ factor(TRATAMENTO),dados_feto) # os grupos apresentam diferença significativa
+
+pairwise.wilcox.test(dados_feto$FÍGADO, factor(dados_feto$TRATAMENTO),
+                     p.adjust.method = "none")
+
+
+#Testando `PC/PF` por TRATAMENTO (Não- paramétrico)
+
+kruskal.test(`PC/PF` ~ factor(TRATAMENTO),dados_feto) # os grupos apresentam diferença significativa
+
+
+`DIAMETRO MEDIO PLACENTA`
+
+# Testando `DIAMETRO MEDIO PLACENTA` por TRATAMENTO (Paramétrico)
+mod <- aov(`DIAMETRO MEDIO PLACENTA` ~ `TRATAMENTO`, data = dados_feto)
+anova(mod)
+
+bonferroni <- LSD.test(mod, "TRATAMENTO", p.adj="bonferroni", group=TRUE)
+print(bonferroni)
+
+
+
+
+####### PLANILHA MÃE GERAL #########
+
+## --------- Analisando mais profundamente os gráficos ------##
+
+# Boxplot PESO 0 por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `PESO 0`)) +
+  geom_boxplot()
+
+# Boxplot PESO FINAL por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `PESO FINAL`)) +
+  geom_boxplot()
+
+# Boxplot `GANHO DE PESO` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `GANHO DE PESO`)) +
+  geom_boxplot()
+
+# Boxplot `N. FETOS` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `N. FETOS`)) +
+  geom_boxplot()
+
+
+# Boxplot `figado (%)` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `figado (%)`)) +
+  geom_boxplot()
+
+# Boxplot FÍGADO por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = FÍGADO)) +
+  geom_boxplot()
+
+
+# Boxplot `baço (%)` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `baço (%)`)) +
+  geom_boxplot()
+
+# Boxplot BAÇO por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = BAÇO)) +
+  geom_boxplot()
+
+
+# Boxplot `G ABD (%)` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `G ABD (%)`)) +
+  geom_boxplot()
+
+
+# Boxplot `G ABDOMINAL` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `G ABDOMINAL`)) +
+  geom_boxplot()
+
+# Boxplot `GGD (%)` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `GGD (%)`)) +
+  geom_boxplot()
+
+
+# Boxplot `GG D.` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `GG D.`)) +
+  geom_boxplot()
+
+
+# Boxplot `GGE (%)` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `GGE (%)`)) +
+  geom_boxplot()
+
+
+# Boxplot `GG ES.` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `GG ES.`)) +
+  geom_boxplot()
+
+# Boxplot `OVA D (%)` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `OVA D (%)`)) +
+  geom_boxplot()
+
+# Boxplot `OVÁRIO D.` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `OVÁRIO D.`)) +
+  geom_boxplot()
+
+# Boxplot `OVA E (%)` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `OVA E (%)`)) +
+  geom_boxplot()
+
+# Boxplot `OVÁRIO E.` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `OVÁRIO E.`)) +
+  geom_boxplot()
+
+
+# Boxplot `N. TENTATIVAS COPULA` por grupo
+ggplot(data = dados_mae, mapping = aes(x = `GRUPO`, y = `N. TENTATIVAS COPULA`)) +
+  geom_boxplot()
+
+
+## ------------ Gráficos de Barras de Erros ------------##
+
+
+# Gráfico de barras de erro para "PESO 0" por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `PESO 0`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `PESO 0`),
+           stat = "summary",
+           fun = "mean")
+
+# Gráfico de barras de erro para `PESO FINAL` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `PESO FINAL`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `PESO FINAL`),
+           stat = "summary",
+           fun = "mean")
+
+
+# Gráfico de barras de erro para `GANHO DE PESO` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `GANHO DE PESO`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `GANHO DE PESO`),
+           stat = "summary",
+           fun = "mean")
+
+
+# Gráfico de barras de erro para `GANHO/FETO` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `GANHO/FETO`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `GANHO/FETO`),
+           stat = "summary",
+           fun = "mean")
+
+# Gráfico de barras de erro para `N. FETOS` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `N. FETOS`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `N. FETOS`),
+           stat = "summary",
+           fun = "mean")
+
+# Gráfico de barras de erro para `figado (%)` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `figado (%)`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `figado (%)`),
+           stat = "summary",
+           fun = "mean")
+
+
+# Gráfico de barras de erro para `FÍGADO` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `FÍGADO`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `FÍGADO`),
+           stat = "summary",
+           fun = "mean")
+
+
+# Gráfico de barras de erro para `baço (%)` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `baço (%)`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `baço (%)`),
+           stat = "summary",
+           fun = "mean")
+
+
+# Gráfico de barras de erro para `BAÇO` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `BAÇO`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `BAÇO`),
+           stat = "summary",
+           fun = "mean")
+
+# Gráfico de barras de erro para `G ABD (%)` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `G ABD (%)`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `G ABD (%)`),
+           stat = "summary",
+           fun = "mean")
+
+# Gráfico de barras de erro para `G ABDOMINAL` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `G ABDOMINAL`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `G ABDOMINAL`),
+           stat = "summary",
+           fun = "mean")
+
+# Gráfico de barras de erro para `GGD (%)` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `GGD (%)`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `GGD (%)`),
+           stat = "summary",
+           fun = "mean")
+
+# Gráfico de barras de erro para `GG D.` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `GG D.`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `GG D.`),
+           stat = "summary",
+           fun = "mean")
+
+# Gráfico de barras de erro para `GGE (%)` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `GGE (%)`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `GGE (%)`),
+           stat = "summary",
+           fun = "mean")
+
+
+# Gráfico de barras de erro para `GG ES.` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `GG ES.`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `GG ES.`),
+           stat = "summary",
+           fun = "mean")
+
+# Gráfico de barras de erro para `OVA D (%)` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `OVA D (%)`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `OVA D (%)`),
+           stat = "summary",
+           fun = "mean")
+
+
+# Gráfico de barras de erro para `OVÁRIO D.` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `OVÁRIO D.`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `OVÁRIO D.`),
+           stat = "summary",
+           fun = "mean")
+
+
+# Gráfico de barras de erro para `OVA E (%)` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `OVA E (%)`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `OVA E (%)`),
+           stat = "summary",
+           fun = "mean")
+
+
+# Gráfico de barras de erro para `OVÁRIO E.` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `OVÁRIO E.`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `OVÁRIO E.`),
+           stat = "summary",
+           fun = "mean")
+
+# Gráfico de barras de erro para `N. TENTATIVAS COPULA` por Grupo
+
+ggplot(data = dados_mae) +
+  geom_errorbar(mapping = aes(x = `GRUPO`, y = `N. TENTATIVAS COPULA`), 
+                stat = "summary",
+                fun.data = "mean_se") +
+  geom_bar(mapping = aes(x = `GRUPO`, y = `N. TENTATIVAS COPULA`),
+           stat = "summary",
+           fun = "mean")
+
+
+##### Filtrando os grupos na planilha mãe geral #####
+
+dados_coco <- dados_mae %>% filter(GRUPO == "COCONUT")
+
+descritiva_coco <- dados_coco %>% get_summary_stats(`PESO 0`,`PESO FINAL`,`GANHO DE PESO`,`GANHO/FETO`,
+                                                    `N. FETOS`,`figado (%)`,`FÍGADO`,`baço (%)`,
+                                                    `BAÇO`,`G ABD (%)`,`G ABDOMINAL`,`GGD (%)`,
+                                                    `GG D.`,`GGE (%)`,`GG ES.`,`OVA D (%)`,`OVÁRIO D.`,
+                                                    `OVA E (%)`,`OVÁRIO E.`,`N. TENTATIVAS COPULA`)
+descritiva_coco
+
+
+dados_cont <- dados_mae %>% filter(GRUPO == "CONTROLE")
+
+descritiva_cont <- dados_cont %>% get_summary_stats(`PESO 0`,`PESO FINAL`,`GANHO DE PESO`,`GANHO/FETO`,
+                                                    `N. FETOS`,`figado (%)`,`FÍGADO`,`baço (%)`,
+                                                    `BAÇO`,`G ABD (%)`,`G ABDOMINAL`,`GGD (%)`,
+                                                    `GG D.`,`GGE (%)`,`GG ES.`,`OVA D (%)`,`OVÁRIO D.`,
+                                                    `OVA E (%)`,`OVÁRIO E.`,`N. TENTATIVAS COPULA`)
+
+descritiva_cont
+
+
+
+dados_lard <- dados_mae %>% filter(GRUPO == "LARD")
+
+descritiva_lard <- dados_lard %>% get_summary_stats(`PESO 0`,`PESO FINAL`,`GANHO DE PESO`,`GANHO/FETO`,
+                                                    `N. FETOS`,`figado (%)`,`FÍGADO`,`baço (%)`,
+                                                    `BAÇO`,`G ABD (%)`,`G ABDOMINAL`,`GGD (%)`,
+                                                    `GG D.`,`GGE (%)`,`GG ES.`,`OVA D (%)`,`OVÁRIO D.`,
+                                                    `OVA E (%)`,`OVÁRIO E.`,`N. TENTATIVAS COPULA`)
+
+
+descritiva_lard
+
+dados_soy <- dados_mae %>% filter(GRUPO == "SOYBEAN")
+
+descritiva_soy <- dados_soy %>% get_summary_stats(`PESO 0`,`PESO FINAL`,`GANHO DE PESO`,`GANHO/FETO`,
+                                                  `N. FETOS`,`figado (%)`,`FÍGADO`,`baço (%)`,
+                                                  `BAÇO`,`G ABD (%)`,`G ABDOMINAL`,`GGD (%)`,
+                                                  `GG D.`,`GGE (%)`,`GG ES.`,`OVA D (%)`,`OVÁRIO D.`,
+                                                  `OVA E (%)`,`OVÁRIO E.`,`N. TENTATIVAS COPULA`)
+
+
+descritiva_soy
+
+##-------- QQplot para as variáveis --------##
+
+# QQ Normal Plot do ggpubr
+ggqqplot(dados_mae$`PESO 0`)
+ggqqplot(dados_mae$`PESO FINAL`)
+ggqqplot(dados_mae$`GANHO DE PESO`)
+ggqqplot(dados_mae$`GANHO/FETO`)
+ggqqplot(dados_mae$`N. FETOS`)
+ggqqplot(dados_mae$`figado (%)`)
+ggqqplot(dados_mae$`FÍGADO`)
+ggqqplot(dados_mae$`baço (%)`)
+ggqqplot(dados_mae$`BAÇO`)
+ggqqplot(dados_mae$`G ABD (%)`)
+ggqqplot(dados_mae$`G ABDOMINAL`)
+ggqqplot(dados_mae$`GGD (%)`)
+ggqqplot(dados_mae$`GG D.`)
+ggqqplot(dados_mae$`GGE (%)`)
+ggqqplot(dados_mae$`GG ES.`)
+ggqqplot(dados_mae$`OVA D (%)`)
+ggqqplot(dados_mae$`OVÁRIO D.`)
+ggqqplot(dados_mae$`OVA E (%)`)
+ggqqplot(dados_mae$`OVÁRIO E.`)
+ggqqplot(dados_mae$`N. TENTATIVAS COPULA`)
+
+
+##-------- Teste de Normalidade para as variáveis --------##
+
+
+# Teste de Normalidade Shapiro
+shapiro.test(dados_mae$`PESO 0`) #Apresenta Normalidade
+shapiro.test(dados_mae$`PESO FINAL`)#Apresenta Normalidade
+shapiro.test(dados_mae$`GANHO DE PESO`)#Apresenta Normalidade
+shapiro.test(dados_mae$`GANHO/FETO`)#Apresenta Normalidade
+shapiro.test(dados_mae$`N. FETOS`)#Apresenta Normalidade
+shapiro.test(dados_mae$`figado (%)`)#Apresenta Normalidade
+shapiro.test(dados_mae$`FÍGADO`)#Apresenta Normalidade
+shapiro.test(dados_mae$`baço (%)`)#Apresenta Normalidade
+shapiro.test(dados_mae$`BAÇO`)#Apresenta Normalidade
+shapiro.test(dados_mae$`G ABD (%)`)#Apresenta Normalidade
+shapiro.test(dados_mae$`G ABDOMINAL`)#Apresenta Normalidade
+shapiro.test(dados_mae$`GGD (%)`)#Apresenta Normalidade
+shapiro.test(dados_mae$`GG D.`)#Apresenta Normalidade
+shapiro.test(dados_mae$`GGE (%)`)#Apresenta Normalidade
+shapiro.test(dados_mae$`GG ES.`)#Apresenta Normalidade
+shapiro.test(dados_mae$`OVA D (%)`)#Apresenta Normalidade
+shapiro.test(dados_mae$`OVÁRIO D.`)#Apresenta Normalidade
+shapiro.test(dados_mae$`OVA E (%)`)# Não apresenta Normalidade
+shapiro.test(dados_mae$`OVÁRIO E.`)# Não apresenta Normalidade
+shapiro.test(dados_mae$`N. TENTATIVAS COPULA`)# Não apresenta Normalidade
+
 
 
 
